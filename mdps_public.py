ee97c71a92d7475f31448ba45d0b0761b8fe464b
Emmanuel A Adeyemo <138520469+DataScienceCoach@users.noreply.github.com>
2024-06-01 09:57:43 -0500

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -43,7 +43,7 @@ with st.sidebar:
 if (selected == 'Diabetes Prediction'):
     
     # page title
-    st.title('Diabetes Prediction using ML')
+    st.title('GM Analytics Diabetes Prediction AI')
     
     
     # getting the input data from the user
@@ -96,7 +96,7 @@ if (selected == 'Diabetes Prediction'):
 if (selected == 'Heart Disease Prediction'):
     
     # page title
-    st.title('Heart Disease Prediction using ML')
+    st.title('GM Analytics Heart Disease Prediction AI')
     
     col1, col2, col3 = st.columns(3)
     
@@ -164,7 +164,7 @@ if (selected == 'Heart Disease Prediction'):
 if (selected == "Parkinsons Prediction"):
     
     # page title
-    st.title("Parkinson's Disease Prediction using ML")
+    st.title("GM Analytics Parkinson's Disease Prediction AI")
     
     col1, col2, col3, col4, col5 = st.columns(5)  
     
